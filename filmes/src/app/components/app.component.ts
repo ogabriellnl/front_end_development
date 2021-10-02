840045ed85daf0a580e4bb195822b1458c1d72ed
Gabriel Lima [SoftVision] <gabriel.lima@softvision.com.br>
2021-10-02 14:04:29 -0300

    Criação do template HMTL com os grids e aplicações de classes Css.

--- filmes/src/app/components/app.component.ts
+++ filmes/src/app/components/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FilmesService } from '../services/filmes.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'filmes';
+
+  constructor(
+    private oFilmesService: FilmesService
+  ) { }
 }
